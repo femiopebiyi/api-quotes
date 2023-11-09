bdfb4a149b15ea8525bd2a299fa5bf35fb29eeb1
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2023-11-09 15:51:50 +0100

    speech problems 6

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ function readAloud (){
     words = words.replace(/\.$/, '')
 let speech = new SpeechSynthesisUtterance (words + " by" + author.textContent)
     speech.lang = 'en-GB';
-    speech.rate = 0.8;
+    speech.rate = 0.95;
 
     speechSynthesis.speak(speech)
 }
