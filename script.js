f7f1d00405ccfcf8f2ac77069cd5b1747e2f902f
Opebiyi Oluwafemi <opebiyibiodun10@gmail.com>
2023-11-09 15:11:35 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+const quoteText =  document.querySelector(".quote"),
+quoteBtn = document.querySelector("button"),
+author =  document.querySelector(".name");
+
+const buttons = document.querySelectorAll("li");
+
+
+
+quoteBtn.addEventListener("click", function(){
+    randomQuote()
+})
+
+randomQuote()
+
+async function randomQuote (){
+    try{
+        quoteBtn.innerHTML = 'Loading'
+        let quoteGeneration = await fetch("https://quotable.io/random")
+        let quote = await quoteGeneration.json()
+        quoteText.innerHTML = quote.content
+        author.innerHTML = quote.author
+        quoteBtn.innerHTML = 'New Quote'
+    }
+    catch(error) {
+        console.log(error , "eror laoding quote")
+    }
+    
+}
+
+buttons[0].addEventListener ("click", function(){
+    readAloud()
+})
+
+function readAloud (){
+let speech = new SpeechSynthesisUtterance (quoteText.textContent + "by" + author.textContent)
+    speech.lang = 'en-GB';
+
+    speechSynthesis.speak(speech)
+}
+
+buttons[1].addEventListener("click", function(){
+    navigator.clipboard.writeText(quoteText.textContent)
+})
+
+buttons[2].addEventListener ("click", ()=>{
+    let tweetUrl = `https://twitter.com/intent/tweet?url=${quoteText.textContent}`;
+
+    window.open(tweetUrl, "_blank")
+})
